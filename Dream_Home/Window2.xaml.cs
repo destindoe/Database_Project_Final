b7e53ad4a124f5e7bffaed55d1daae036653bf1b
Jonathan Jones <jj06328@georgiasouthern.edu>
2018-04-22 13:28:31 -0400

    Most Current Design and Functionality

--- Dream_Home/Window2.xaml.cs
+++ Dream_Home/Window2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+
 
 namespace Dream_Home
 {
@@ -19,6 +22,8 @@ namespace Dream_Home
     /// </summary>
     public partial class Window2 : Window
     {
+        //Grab connection
+        SqlConnection connection = ConnectionManager.Open(ConnectionStrings.Destin);
         public Window2()
         {
             InitializeComponent();
@@ -29,6 +34,54 @@ namespace Dream_Home
             new Login().Show();
             this.Close();
           
+        }
+
+        private void BDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DataGrid gd = (DataGrid)sender;
+            DataRowView row_selected = gd.SelectedItem as DataRowView;
+
+            if (row_selected != null)
+            {
+                BBranch.Text = row_selected["BranchNo"].ToString();
+                BManager.Text = row_selected["ManagerNo"].ToString();
+                BState.Text = row_selected["State"].ToString();
+                BCity.Text = row_selected["City"].ToString();
+                BStreet.Text = row_selected["Street"].ToString();
+            }
+        }
+
+        private void BAddbtn_Click(object sender, RoutedEventArgs e)
+        {
+            var command = new SqlCommand("Select * From Branch", connection);
+
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            var newRow = dt.NewRow();
+            newRow["BranchNo"] = BBranch.Text;
+            newRow["ManagerNo"] = BManager.Text;
+            newRow["State"] = BState.Text;
+            newRow["City"] = BCity.Text;
+            newRow["Street"] = BStreet.Text;
+            dt.Rows.Add(newRow);
+
+            new SqlCommandBuilder(da);
+            da.Update(dt);
+            BDataGrid.ItemsSource = dt.DefaultView;
+
+        }
+
+        private void staffSearchbtn_Click(object sender, RoutedEventArgs e)
+        {
+            var command = new SqlCommand("Select * From Branch", connection);
+
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            BDataGrid.ItemsSource = dt.DefaultView;
         }
     }
 }
